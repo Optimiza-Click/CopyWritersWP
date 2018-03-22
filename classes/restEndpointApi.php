ee77b1f3c20b70c7bd285c7a2e1e4364910cc664
Luis david del barrio <ldelbarrio@optimizaclick.com>
2018-03-22 11:28:33 +0100

    check if is all in one or seo yoast

--- classes/restEndpointApi.php
+++ classes/restEndpointApi.php
@@ -15,16 +15,51 @@ class restEndpointApi
         ]);
 
         register_rest_route('blogging/v1', '/seo_meta/', [
-            'methods' => 'POST',
+            'methods' => 'GET',
             'callback' => [$this, 'updateSEOMetasByPlugin'],
         ]);
     }
+
+
+    private function transformMetaByWpSeoPlugin($name) {
+        $pluginList = get_option( 'active_plugins' );
+
+        $seoPlugins = [
+            'Yoast' => [
+                'dir' => 'wordpress-seo/wp-seo.php',
+                'seo_title' => '_yoast_wpseo_title',
+                'seo_description' => '_yoast_wpseo_metadesc'
+                ],
+            'AllInOne' => [
+                'dir' => 'all-in-one-seo-pack/all_in_one_seo_pack.php',
+                'seo_title' => '_aioseop_title',
+                'seo_description' => '_aioseop_description'
+                ]
+        ];
+
+
+        foreach($seoPlugins as $key => $seoPlugin ) {
+            if ( in_array( $seoPlugin['dir'] , $pluginList ) ) {
+                switch($name){
+                    case 'seo_title':
+                        return $seoPlugin['seo_title'];
+                    case 'seo_description':
+                        return $seoPlugin['seo_description'];
+                }
+            }
+        }
+    }
+
 
     public function updateSEOMetasByPlugin($request) {
         $params = $request->get_params();
 
         foreach($params['meta'] as $key => $value) {
-            update_post_meta($params['id'], $key, $value);
+            $meta_key = $this->transformMetaByWpSeoPlugin($key);
+
+            if($meta_key) {
+                update_post_meta($params['id'], $meta_key, $value);
+            }
         }
 
         return ['success' => true];
